296f2314a376d1d8d0d86066bf12c2ffebbdd55a
Shadabdullah <shadabdullah101@gmail.com>
2024-02-05 17:27:20 +0530

    Setup Theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const VocabApp());
+  runApp(VocabApp());
 }
 
 class VocabApp extends StatelessWidget {
-  const VocabApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'VOCAB',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("wait"),
+      themeMode: ThemeMode.system, // Use system theme mode (light or dark)
+      darkTheme: ThemeData.dark().copyWith(
+       textTheme: const TextTheme(
+         bodyLarge: TextStyle(color: Colors.yellow),
+       )
+      ), // Use dark theme
+      theme: ThemeData.light().copyWith(
+        textTheme: const TextTheme(
+          // Define text styles for both light and dark themes
+          bodyLarge: TextStyle(color: Colors.green),
+          
+  
         ),
-        body: const Center(
-          child: Text('Hello World'),
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Theme Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Hello, World!',
+              style: Theme.of(context).textTheme.bodySmall, 
+            ),
+           const  SizedBox(height: 20),
+           
+          ],
         ),
       ),
     );
