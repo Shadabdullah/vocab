d44d8c2b68ea2f59888202851df3b903f37b86dc
Shadabdullah <Shadabdullah101@gmail.com>
2024-02-20 19:35:35 +0530

    Card Setup

--- lib/screens/progress_page.dart
+++ lib/screens/progress_page.dart
@@ -1,11 +1,9 @@
-
 import 'package:flutter/material.dart';
 
 class ProgressPage extends StatelessWidget {
   const ProgressPage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.lightGreen, child: Center(child: Text("Progress You have made"),));
+    return Text('Wait');
   }
 }
--- lib/screens/sets_page.dart
+++ lib/screens/sets_page.dart
@@ -1,11 +1,26 @@
-
 import 'package:flutter/material.dart';
+import 'package:vocab/widgets/setcard.dart';
 
 class SetsPage extends StatelessWidget {
-  const SetsPage({super.key});
+  SetsPage({super.key});
+  final List<String> items = List.generate(30, (index) => 'SET $index');
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green, child: Center(child: Text("Sets "),));
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3, crossAxisSpacing: 1),
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) {
+        return AspectRatio(
+          aspectRatio: 1, // Adjust aspect ratio as needed
+          child: SetCard(
+            title: items[index],
+            progress: 75,
+            unlocked: false,
+          ),
+        );
+      },
+    );
   }
 }
